4e7251a1ace63a77dbf89479e978834e6938a32e
Muhammad Salman <chsalmanramzan422@gmail.com>
2026-08-28 00:07:53 -0700

    Implement the game functionality with Abstraction

--- Game_Arena/Enemy.py
+++ Game_Arena/Enemy.py
@@ -3,14 +3,14 @@ class Enemy:
     health_points: int = 100
     attack_damage: int = 1
 
-def talk(self=None):
-    print(f'I am a {self.type_of_enemy}. So be prepared')
+    def talk(self):
+        print(f'I am a {self.type_of_enemy}. So be prepared')
 
 
-def walk_forward(self=None):
-    print(f'{self.type_of_enemy}. Move Closer')
+    def walk_forward(self):
+        print(f'{self.type_of_enemy}. Move Closer')
 
 
-def attack(self=None):
-    print(f'{self.type_of_enemy} attacks for {self.attack_damage} damage')
+    def attack(self):
+        print(f'{self.type_of_enemy} attacks for {self.attack_damage} damage')
 
--- Game_Arena/main.py
+++ Game_Arena/main.py
@@ -2,4 +2,5 @@ from Enemy import *
 enemy= Enemy()
 enemy.type_of_enemy="Zombie"  #this was a quick fix, coz we didn't assign or add any field for the type_of_enemy
 
-print(f'{enemy.type_of_enemy} has {enemy.health_points} health and the attack damage{enemy.attack_damage}')
+print(enemy.walk_forward())
+# print(f'{enemy.type_of_enemy} has {enemy.health_points} health and the attack damage{enemy.attack_damage}')
