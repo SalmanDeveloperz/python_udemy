5d4beb466d5b4ca7f10af783e0ddc1bd21563e0d
Muhammad Salman <chsalmanramzan422@gmail.com>
2026-08-28 04:53:06 -0700

    Added the Encapsulation and Abstraction, The pillars of OOP
    Signed-off-by: Muhammad Salman <chsalmanramzan422@gmail.com>

--- Game_Arena/Enemy.py
+++ Game_Arena/Enemy.py
@@ -3,9 +3,12 @@ class Enemy:
     health_points: int = 100
     attack_damage: int = 1
 
+
+    def __init__(self, type_of_enemy, health_points,attack_damage):
+        print("Create new Enemy")
+
     def talk(self):
         print(f'I am a {self.type_of_enemy}. So be prepared')
-
 
     def walk_forward(self):
         print(f'{self.type_of_enemy}. Move Closer')
