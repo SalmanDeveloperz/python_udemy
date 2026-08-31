31a8078ce164677c4e91c76f9ade95e4b579586b
Muhammad Salman <151463017+SalmanDeveloperz@users.noreply.github.com>
2026-08-31 03:07:06 -0700

    Update books.py

--- fastAPI/books.py
+++ fastAPI/books.py
@@ -5,7 +5,7 @@ app= FastAPI()
 BOOKS=[
     {'title': 'Title One', 'author': 'Muhammad', 'category': 'Spirtual'},
     {'title': 'Title two', 'author': 'Salman', 'category': 'Science'},
-    {'title': 'Title One', 'author': 'Ahsan', 'category': 'Mathematics'},
+    {'title': 'Title One', 'author': 'Ahsan', 'category': 'Spirtual'},
     {'title': 'Title One', 'author': 'Umar', 'category': 'English'},
     {'title': 'Title One', 'author': 'Zain', 'category': 'Computer'}
 
@@ -22,3 +22,16 @@ async def book_reading():
 async def read_all_books(dynamic_param: str):
     return{'dynamic_param': dynamic_param}
 
+@app.get("/books/{book_title}")
+async def read_book_in_loop(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
+
+
+async def read_books_category(category:str):
+    books_return=[]
+    for i in BOOKS:
+        if i.get('category').casefold()== category.casefold():
+            books_return.append(i)
+        return book_return
