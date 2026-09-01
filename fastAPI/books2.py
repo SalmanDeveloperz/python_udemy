cd447faf47318e4f4e6b21bb51e2869d82831178
Muhammad Salman <151463017+SalmanDeveloperz@users.noreply.github.com>
2026-09-01 03:36:52 -0700

    Enhance ValidRequest model and unique ID assignment
    Updated ValidRequest model to include field validations for title, author, description, and rating. Modified create_book function to assign unique IDs to new books.

--- fastAPI/books2.py
+++ fastAPI/books2.py
@@ -21,17 +21,17 @@ class Book:
 
 class ValidRequest(BaseModel):
     id: int
-    title: str
-    author: str
-    description: str
-    rating: int
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=3, max_length=100)
+    rating: int = Field(gt=-1, lt=6)  #greater than -1 and less than 6
 
 BOOKS=[
     Book(1,'computer Science', 'M-Salman', 'Computer Fundamentals', '3'),
     Book(2, 'Python', 'Numan', 'Python OOP Fundamentals', '5'),
     Book(3, 'Mathematics', 'Naveed', 'Operations', '5'),
     Book(4, 'English', 'Hassan Tariq', 'IELTS', '3'),
-    Book(5, 'Chemistry', 'Saadullah', 'Organic Chemistry', '4')
+    Book(5, 'Chemistry', 'Saad', 'Organic Chemistry', '4')
 
 ]
 
@@ -50,7 +50,14 @@ POST
 async def create_book(book_request: ValidRequest):
     new_book= Book(**book_request.model_dump())
     print(type(new_book))
-    BOOKS.append(new_book)
+    BOOKS.append(unique_book_id(new_book))
 
 
+# Let's create the Primary or Unique id for every iteration
+def unique_book_id(i:Book):
+    if len(BOOKS)>0:
+        i.id= BOOKS[-1].id+1
+    else:
+        i.id=1
 
+    return i
