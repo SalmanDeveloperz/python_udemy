913aaedae7b050380b0afff1df68710b040651ee
Muhammad Salman <151463017+SalmanDeveloperz@users.noreply.github.com>
2026-09-02 23:07:29 -0700

    Implement database session handling in FastAPI app
    Added database session management and dependency injection for FastAPI.

--- todoApp/main.py
+++ todoApp/main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends
 import models
-from database import engine
+from models import Todos
+from database import engine, SessionLocal
 
 app= FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=engine) # this only run if todos.db not exist
+
+def get_db():
+    db=SessionLocal()
+    try:
+        yield db #yield means the code prior to and including yield statement will executed before sending response
+
+    finally:
+        db.close() #this only executed after response has been delivered, make fastAPI quicker, very safe ,close connection in the end
+
+db_dependency= Annotated[Session, Depends(get_db)] #depends is dependency injection really need before we execute behind scenes
+
+@app.get("/")
+async def read_all(db:db_dependency):
+    return db.query(Todos).all()
